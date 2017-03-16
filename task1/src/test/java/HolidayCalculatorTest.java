d596ed208c8d3432960ce281bfbcdb2d122d7a47
anton_veklych <sks14info@gmail.com>
2017-03-16 17:40:12 +0200

    Created tests

---
+++ task1/src/test/java/HolidayCalculatorTest.java
@@ -0,0 +1,58 @@
+import com.aviva.recruitingtasks.kmol.task1.HolidayCalculator;
+import com.aviva.recruitingtasks.kmol.task1.model.Employee;
+import com.aviva.recruitingtasks.kmol.task1.sampledata.MonthException;
+import com.aviva.recruitingtasks.kmol.task1.sampledata.SampleAvivaEmployees;
+import com.aviva.recruitingtasks.kmol.task1.sampledata.SampleAvivaHolidays;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HolidayCalculatorTest {
+    Employee JOHN = new Employee("JOHN");
+    Employee ANNA = new Employee("ANNA");
+    Employee ROB = new Employee("ROB");
+    Employee LUCAS = new Employee("LUCAS");
+
+    HolidayCalculator calculator = new HolidayCalculator(SampleAvivaEmployees.AVIVA_EMPLOYEES, SampleAvivaHolidays.AVIVA_HOLIDAYS);
+    List<Employee> employeeList;
+    Month month;
+
+    @Test
+    public void getWorkingEmployees(){
+
+        employeeList = new ArrayList<>();
+        employeeList.add(JOHN);
+        employeeList.add(ANNA);
+        employeeList.add(ROB);
+        employeeList.add(LUCAS);
+        assertEquals(employeeList, calculator.getWorkingEmployees(LocalDate.of(2016, Month.JANUARY, 1), LocalDate.of(2016, Month.DECEMBER, 31)));
+
+        employeeList = new ArrayList<>();
+        employeeList.add(LUCAS);
+        assertEquals(employeeList, calculator.getWorkingEmployees(LocalDate.of(2017, Month.JANUARY, 1), LocalDate.of(2017, Month.DECEMBER, 31)));
+
+        employeeList = new ArrayList<>();
+        employeeList.add(ANNA);
+        employeeList.add(LUCAS);
+        assertEquals(employeeList, calculator.getWorkingEmployees(LocalDate.of(2017, Month.AUGUST, 8), LocalDate.of(2017, Month.AUGUST, 12)));
+
+        employeeList = new ArrayList<>();
+        employeeList.add(LUCAS);
+        assertEquals(employeeList, calculator.getWorkingEmployees(LocalDate.of(2017, Month.JULY, 13), LocalDate.of(2017, Month.AUGUST, 1)));
+
+    }
+
+    @Test(expected = MonthException.class)
+    public void getHolidayMonth(){
+        month = Month.AUGUST;
+        assertEquals(month, calculator.getHolidayMonth(2017));
+
+        calculator.getHolidayMonth(2010);
+
+    }
+
+}
